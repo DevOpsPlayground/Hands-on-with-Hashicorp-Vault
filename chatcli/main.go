4c56f7b438a8325aba36bd0b41c66270d968f2db
James Sandford <sandford.james@gmail.com>
2022-09-28 10:08:14 +0100

    feat: add code to log in to vault

--- chatcli/main.go
+++ chatcli/main.go
@@ -1,14 +1,12 @@
 package main
 
 import(
-	"context"
-	"log"
-	"time"
+	"net/http"
 	"fmt"
-	"bufio"
-	"os"
-
-	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"net/url"
+	"io"
+	"encoding/json"
 )
 func failOnError(err error, msg string){
 	if err != nil{
@@ -16,74 +14,26 @@ func failOnError(err error, msg string){
 	}
 }
 
-func sendMessage(ch *amqp.Channel, ctx context.Context, msg, queueName string){
-	err := ch.PublishWithContext(ctx,
-		"", //exchange
-		queueName, //routing Key
-		false, //mandatory
-		false, //immediate
-		amqp.Publishing{
-			ContentType: 	"text/plain",
-			Body:			[]byte(msg),
-		},
-	)
-	failOnError(err, "Failed to publish message")
-	
+type userpass struct{
+	Auth auth `json:"auth"`
 }
-
+type auth struct{
+	ClientToken string `json:"client_token"`
+}
 func main(){
-	//Set up
-	username := "guest"
-	password := "guest"
-	url := "localhost"
-	port := "5672"
-	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s",username,password,url,port))
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"hello", // Name
-		false, //durable
-		false, //delete when unused
-		false, //exclusive
-		false, //no-wait
-		nil, //args
-	)
-	failOnError(err, "Failed to declare queue")
-	//Consume message
-	msgs, err := ch.Consume(
-		q.Name, //queue
-		"",		//consumer
-		true,	//auto-ack
-		false,	//exclusive
-		false,	//no-local
-		false,	//no-wait
-		nil,	//args
-	)
-	failOnError(err, "Failed to register consumer")
-
-	var forever chan struct{}
-
-	go func()  {
-		for d := range msgs{
-			log.Printf("%s",d.Body)
-		}
-	}()
-
-	//Send Message
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	
-	for true{
-		scanner := bufio.NewScanner(os.Stdin)
-    	scanner.Scan()
-		sendMessage(ch,ctx,scanner.Text(),q.Name)
-	}
-	
-
-	<-forever
+	vaulturl := "http://localhost:8200"
+	username := "byteford"
+	resp, err := http.PostForm(
+		fmt.Sprintf("%s/v1/auth/userpass/login/%s",vaulturl, username),
+	 	url.Values{"password":{"pass"}})
+	failOnError(err, "failed to log in to vault")
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	failOnError(err, "failed to read body")
+	fmt.Print(string(body))
+	var up userpass
+	err = json.Unmarshal(body,&up)
+	failOnError(err, "failed to Unmarshal body")
+	fmt.Print(up)
+	runRabbit()
 }
---
+++ chatcli/rabbit.go
@@ -0,0 +1,85 @@
+package main
+import(
+	"context"
+	"log"
+	"time"
+	"fmt"
+	"bufio"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+
+func sendMessage(ch *amqp.Channel, ctx context.Context, msg, queueName string){
+	err := ch.PublishWithContext(ctx,
+		"", //exchange
+		queueName, //routing Key
+		false, //mandatory
+		false, //immediate
+		amqp.Publishing{
+			ContentType: 	"text/plain",
+			Body:			[]byte(msg),
+		},
+	)
+	failOnError(err, "Failed to publish message")
+	
+}
+
+
+func runRabbit(){
+	//Set up
+	username := "guest"
+	password := "guest"
+	url := "localhost"
+	port := "5672"
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s",username,password,url,port))
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // Name
+		false, //durable
+		false, //delete when unused
+		false, //exclusive
+		false, //no-wait
+		nil, //args
+	)
+	failOnError(err, "Failed to declare queue")
+	//Consume message
+	msgs, err := ch.Consume(
+		q.Name, //queue
+		"",		//consumer
+		true,	//auto-ack
+		false,	//exclusive
+		false,	//no-local
+		false,	//no-wait
+		nil,	//args
+	)
+	failOnError(err, "Failed to register consumer")
+
+	var forever chan struct{}
+
+	go func()  {
+		for d := range msgs{
+			log.Printf("%s",d.Body)
+		}
+	}()
+
+	//Send Message
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	for true{
+		scanner := bufio.NewScanner(os.Stdin)
+    	scanner.Scan()
+		sendMessage(ch,ctx,scanner.Text(),q.Name)
+	}
+	
+
+	<-forever
+}
